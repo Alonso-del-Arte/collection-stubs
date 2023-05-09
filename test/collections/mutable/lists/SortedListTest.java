4d0bbcd091289e9aaf1465cf73c4cada29499adb
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-09 17:01:40 -0400

    Failing can add to list test

--- test/collections/mutable/lists/SortedListTest.java
+++ test/collections/mutable/lists/SortedListTest.java
@@ -1,5 +1,7 @@
 package collections.mutable.lists;
 
+import static collections.CollectionTest.RANDOM;
+
 import java.util.Date;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,14 @@ class SortedListTest {
         SortedList<Date> list = new SortedList<>();
         String msg = "Newly created list with no elements should be empty";
         assert list.isEmpty() : msg;
+    }
+
+    @Test
+    void testCanAddToList() {
+        SortedList<String> list = new SortedList<>();
+        String msg = "Should have been able to add this message to the list";
+        boolean opResult = list.add(msg);
+        assert opResult : msg;
     }
 
 }
