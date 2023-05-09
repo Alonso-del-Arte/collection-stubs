b433012b2dab5f47d132f32e2e96e64cfc2dac53
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-09 16:55:58 -0400

    Failing newly created list should be empty test

--- test/collections/mutable/lists/SortedListTest.java
+++ test/collections/mutable/lists/SortedListTest.java
@@ -1,8 +1,17 @@
 package collections.mutable.lists;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortedListTest {
+
+    @Test
+    void testNewListShouldBeEmpty() {
+        SortedList<Date> list = new SortedList<>();
+        String msg = "Newly created list with no elements should be empty";
+        assert list.isEmpty() : msg;
+    }
 
 }
