bb3a705cb68647403fae6a63255fca6093abad3a
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-08 23:59:40 -0500

    Starting to flesh out

--- src/collections/mutable/maps/Map.java
+++ src/collections/mutable/maps/Map.java
@@ -3,4 +3,13 @@ package collections.mutable.maps;
 // TODO: Look at Scala maps to figure out how to put these in the collections
 //  hierarchy
 public abstract class Map<K, V> {
+
+    public abstract boolean isEmpty();
+
+    public abstract int size();
+
+    public abstract void clear();
+
+    public abstract boolean containsKey(K key);
+
 }
